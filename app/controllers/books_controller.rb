796e54ce5e14ad8375e73030890c013bc827facf
EC2 Default User <ec2-user@ip-172-31-9-250.ec2.internal>
2022-01-12 01:06:45 +0000

    [Fix]新規登録機能の作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,14 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
-
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to 'books/index'
+  end
+    
   def index
   end
 
@@ -10,4 +17,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
