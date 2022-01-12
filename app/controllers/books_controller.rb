7cb7b397642f58b5dac1bfa66d6c166078d6ab18
EC2 Default User <ec2-user@ip-172-31-9-250.ec2.internal>
2022-01-12 01:27:00 +0000

    [Fix]bookers 新規投稿画面修正完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,13 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
     redirect_to 'books/index'
   end
-    
+
   def index
   end
 
@@ -17,10 +17,10 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
